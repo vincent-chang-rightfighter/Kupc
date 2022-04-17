10e703d80c696293b080cf55dfa0c24fcb8c6cb7
vincent <vincent870922@gmail.com>
2022-04-17 14:10:43 +0800

    fix submodules theme

--- CopyThemeFileToAppdata.bat
+++ CopyThemeFileToAppdata.bat
@@ -1,5 +1,5 @@
-@echo off
-xcopy sun-valley.tcl %AppData%\KUPC\sun-valley.tcl*
-xcopy /E /I theme\ %AppData%\KUPC\theme\
+@REM @echo off
+xcopy  /I "Sun-Valley-ttk-theme\sun-valley.tcl" %AppData%\KUPC\sun-valley.tcl*
+xcopy /E /I Sun-Valley-ttk-theme\theme\ %AppData%\KUPC\theme\
 echo 主題複製完畢
 timeout /t 5 /nobreak
